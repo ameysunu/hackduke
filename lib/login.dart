1f415887cf1db0bb2a0b6d9cdeec9954482abde6
Amey Sunu <ameysunu00719@gmail.com>
2020-12-05 20:10:01 +0300

    login ui updated

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -9,8 +10,75 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Column(),
+      backgroundColor: HexColor('#000000'),
+      appBar: AppBar(
+        title: Text("Login"),
+        backgroundColor: HexColor('#000000'),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "Get Started ➜",
+                style: TextStyle(fontSize: 30, color: HexColor('#FFFFFF')),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: HexColor('#9296F0'),
+                child: Container(
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          height: 25,
+                          width: 25,
+                          child: Image.asset("images/google.png"),
+                        ),
+                      ),
+                      Text(
+                        "Sign in with Google",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: "Roboto Medium",
+                            fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                onPressed: () {
+                  null;
+                },
+              ),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "By signing in, you agree to Google's terms and conditions.",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: HexColor('#6F75FA'),
+                        fontStyle: FontStyle.italic),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
